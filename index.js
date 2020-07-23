d62984e2a667a2132776886d2cf2bc37339dfc93
tenhold <tenhold@gmail.com>
2020-07-22 22:21:47 -0500

    "v2"

--- index.js
+++ index.js
@@ -7,47 +7,98 @@ $(document).ready(() => {
 
 ///////////          Creating a main list in the body           ///////////
 
+
+// put the tweets in a div 
   
 
   const $mainDiv = $('<div></div>')
     .attr('class', 'main')
     .attr('id', 'main')
     .appendTo($('#top'));
-  const $twiddleList = $('<ul>')
-    .attr('class', 'twiddle-list')
-    .attr('id', 'twiddle-list')
-    .appendTo($mainDiv);
+  
 
 
 
 ///////////           Populating the last with twidders         ///////////
+  
+console.log('streams', streams)
+console.log('home', streams.home)
 
-
-
-  const $tweets = streams.home.map((tweet) => {
+const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>')
-      .attr('id', 'twiddle');
-
-    let fromNow = moment().startOf(tweet.created_at).fromNow();
-    const user = tweet.user;
-    const text = `@${tweet.user}: ${tweet.message} at ${fromNow}`;
-
+      .attr('class', 'tweet')
+      .addClass('tweet');
+    const $user = $('<a>')
+      .attr('class', 'user')
+      .append(tweet.user);
+    const $message = $('<p></p>')
+      .attr('class', 'message')
+      .append(tweet.message);
     
 
+    let fromNow = moment().startOf(tweet.created_at).fromNow();
+ 
+    const text = `@${tweet.user}: ${tweet.message} at ${fromNow}`;
+
+    $mainDiv.append($tweet)
+
+    $tweet.append($user, $message);
     $tweet.text(text)
     return $tweet;
   });
 
   $mainDiv.append($tweets);
 
+  console.log($tweets)
+
+
+  // const $tweets = streams.home.map((tweet) => {
+  //   const $tweet = $('<div></div>')
+  //     .attr('class', 'tweet')
+  //     .addClass('tweet');
+  //   const $user = $('<a>')
+  //     .attr('class', 'user')
+  //     .append(tweet.user);
+  //   const $message = $('<p></p>')
+  //     .attr('class', 'message')
+  //     .append(tweet.message);
+
+  //   let fromNow = moment().startOf(tweet.created_at).fromNow();
+ 
+  //   const text = `@${tweet.user}: ${tweet.message} at ${fromNow}`;
+
+  //   $mainDiv.append($tweet)
+
+  //   $tweet.append($user, $message);
+  //   $tweet.text(text)
+  //   return $tweet;
+  // });
+
+  // $mainDiv.append($tweets);
+
+
   ///////////        Add to list                             ///////////
 
-  $('#submit').submit(function(e) {
-    alert('submit called');
-    localStorage.setItem('twid', $('#twid'));
-    e.preventDefault();
-  })
 
+
+        // Continually run to update count of new, not displayed, tweets
+        (function updateTweetCount(){
+          if(streams.home.offset > 0){
+            var newTweetsCount = 0;
+            if(location.hash && location.hash.substr(1) !== 'timeline'){
+              var user = location.hash.substr(1);
+              newTweetsCount = streams.users[user].length - 
+                $('.tweet').filter('[data-user="' + user + '"]').length;
+            }else{
+              newTweetsCount = streams.home.length - streams.home.offset;
+            }
+            if(newTweetsCount > 0){
+              $('#newtweets-count').text(newTweetsCount);
+              $('#timeline-newtweets').slideDown(300);
+            }
+          }
+          setTimeout(updateTweetCount, 5000);
+        })();
 
   // $('text').keyup(function(e) {
   //   let code = e.which;
@@ -59,17 +110,16 @@ $(document).ready(() => {
 
 
 
-  const inpTwid = document.getElementById($('#text'));
+  // const inpTwid = document.getElementById($('#text'));
 
-  const btn = document.getElementById($('#submit'));
-
-  const tweetList = document.getElementById($twiddleList);
+  // const btn = document.getElementById($('#submit'));
 
 
 
 
 
 
+  
 
 
 
