3fa3161b7cac2f1ec93a501463ae25913255bc26
tenhold <tenhold@gmail.com>
2020-07-26 08:14:37 -0500

    "working on the user tweet"

--- data-generator.js
+++ data-generator.js
@@ -68,15 +68,16 @@ scheduleNextTweet();
 // Utility function for letting students add "write a tweet" functionality
 // (NOTE: Not used by the rest of this file.)
 const writeTweet = (message) => {
-  // const visitor = window.visitor;
+  const visitor = window.visitor;
 
-  // if (!visitor){
-  //   throw new Error('Set the global visitor property!');
-  // }
+  if (!visitor){
+    throw new Error('Set the global visitor property!');
+  }
 
   const tweet = {
     user: visitor,
     message: message,
   };
+  console.log(visitor)
   addTweet(tweet);
 };
--- index.js
+++ index.js
@@ -2,30 +2,39 @@
 
 
 
-const makeTweet = function(me) {
+// const makeTweet = function(me) {
 
 
-  const tweets = $(document.createElement('div')).addClass('tweet');
-  const h3 = $(document.createElement('h3')).text(me.userName);
-  const p = $(document.createElement('p')).text(me.tweet);
+//   const $tweet = $(document.createElement('div'))
+//     .addClass('tweet')
+//     .attr('id', me.userName)
+//     .attr('data-date', tweet.created_at)
+//     .text(Date())
+//   const $name = $(document.createElement('a'))
+//     .text(me.userName);
+//   const $message = $(document.createElement('div'))
+//     .text(me.message);
+//   const $time = $(document.createElement('div'))
+//     .addClass('tweet')
 
-  return (tweets.append(h3).append(p)).prop('outerHTML');
+//   // return (tweets.append(h3).append(p)).prop('outerHTML');
+// }
 
+// const newUser = {
+//   user: 'jon',
+//   message: 'hello'
+// }
 
-}
-
-
+$(document).on('click', '#twit-it', function() {
+  const tweet = $(this).siblings('input')
+  console.log(tweet)
+})
 
 $(document).ready(() => {
   const $body = $('body');
   const $mainDiv = $('.main');
-  // $body.html('');
 
-  console.log('stream', streams)
-  console.log('home', streams.home);
-
-
-const $homeBtn = $('#to-home');
+  const $homeBtn = $('#to-home');
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -33,10 +42,10 @@ const $homeBtn = $('#to-home');
 ///////////////////////////////////////////////////////////////////////////
 
 
-getTweets();
+getTweets(streams.home);
 
-function getTweets() {
-  const $tweets = streams.home.map((tweet) => {
+function getTweets(array) {
+  const $tweets = array.map((tweet) => {
     const $tweet = $(document.createElement('div'))
       .addClass('tweet')
       .attr('id', tweet.user)
@@ -54,7 +63,7 @@ function getTweets() {
     const $time = $(document.createElement('div'))
       .addClass('time')
       .attr('id', 'time')
-      .text(friendlyTimer)
+      .text(`twidded ${time}`)
     $tweet.append($user, $message, $time);
     $tweet.prependTo($('.main'));
     return $tweet;
@@ -65,44 +74,22 @@ function getTweets() {
 
 const $tweet = $('.tweet');
 
-// console.log($tweet[0].dataset.date)
+// console.log($tweet[0].dataset.date, $tweet)
 
 ////////////////////////////////////////////////////////////////////////////
 //                   get current time on tweets                          //
 ///////////////////////////////////////////////////////////////////////////
 
 
-const updateTimers = () => {
-  $(".main").each(function() {
-    const $getIndex = _.last($(this).attr('class').split(' '));
-    let msPassed = streams.home[$getIndex].created_at - moment();
-    let friendlyTimer = moment.duration(msPassed, "milliseconds").humanize(true);
-    $(this).children($(".friendly-tag")).text(`Borked ${friendlyTimer}`)
+function updateTweetTime(){
+  $('.tweet').each(function(){   
+    let newTime = $(this).data('date');
+    newTime = moment(newTime).fromNow(true);
+    $(this).find('.time').text(`twidded ${newTime}`);
   });
-  setTimeout(updateTimers, 3000);
-}
-updateTimers();
+};
 
-// (function newTime() {
-
-//   setInterval(function() {
-//     streams.home.forEach((user) => {
-//       // $tweet.dataset.date
-//       // $tweet.dataset.date = moment(user.created_at, 'ddd mmm dd yyyy HH:MM:ss').fromNow(true);
-
-//     });
-//   }, 2000);
-// })();
-
-
-// setInterval(function() {
-//   $tweet.each(function() {
-//     let newTime = this.dataset.date;
-//     newTime = moment().diff(newTime)
-//     $(this).find('.tweet-date').text(newTime);
-//   })  
-  
-// }, 2000);
+setInterval(updateTweetTime, 30000);
 
 ////////////////////////////////////////////////////////////////////////////
 //                   generate random tweets with click                    //
@@ -110,7 +97,12 @@ updateTimers();
 
 
 $('#get-new-tweet').on('click', function() {    
-  getTweets();
+  const curTweets = $('.tweet').length;
+  const undisplayedTweets = streams.home.slice(curTweets)
+  if (undisplayedTweets.length !== 0) {
+    getTweets(undisplayedTweets);
+  }
+  return false;
 })
 
 
@@ -134,14 +126,18 @@ $('body').on('click', '.user', function() {
   
   $filteredTweets.hide();
   $homeBtn.show();
+  $('#get-new-tweet').hide();
+  $('#user-home').show();
+  $('#user-home').text(`${$clickedName} twids`);
 
   $homeBtn.on('click', function() {
     $filteredTweets.show();
     $homeBtn.hide();
+    $('#get-new-tweet').show();
+    $('#user-home').hide()
+
     return false;
   })
-
-
   
   return false;
 });
@@ -168,51 +164,21 @@ $('body').on('click', '.user', function() {
 
 // on click run the fucntion to get input values that will add the 
 // new user and the tweet to the streams.users object
-  $('#twit-it').on('click', function() {
-    const username = $(this).siblings('input').val();
-    const newTweet = $('#twit').val();
+  // $('#twit-it').on('click', function() {
+  //   const username = $(this).siblings('input').val();
+  //   const newTweet = $('#twit').val();
 
-    streams.users[username] = [];
-    streamss.users
-    // addTweet(newTweet)
+  //   streams.users[username] = [];
+  //   streams.users
+  //   // addTweet(newTweet)
 
-    return false;
-  })
+  //   return false;
+  // })
 
   // function getInputValue() {
   //   const username = document.getElementById('user-name').value;
   //   streams.users[newUser] = [newTweet];
   // }
 
-
-
-/////////////////////////////////////////////////////////////////////
-//                     update time                                //
-////////////////////////////////////////////////////////////////////
-
-
-// function updateTime() {
-//   streams.home.forEach(function(user) {
-//     let curTime = moment(users.created_at).format('HH:mm:ss a');
-//     ($mainDiv).html('current tweet' + curTime)
-//   })
-// }
-
-// updateTime();
-// setInterval(function() {
-//   updateTime();
-// }, 1000)
-  
-    // setTimeout(function (){
-    //   $(document.createElement('a'))
-    //     .addClass('get-more')
-    //     .attr('href', '#')
-    //     .text('2 new tweets')
-    //     .prependTo('#tweets');
-  
-  
-    // }, 1000);
-  
-   
 
 });
